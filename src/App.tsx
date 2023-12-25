860a2e8cc17e0c36102d28a4b5a56742b22631a6
prathmesh <prathmeshpol61@gmail.com>
2023-12-25 10:43:21 +0530

    starter code

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center gap-y-5">
+      <h1 className="font-bold text-xl text-clip">
+        Password Generator Optimize App
+      </h1>
+    </div>
+  );
+};
+
+export default App;
